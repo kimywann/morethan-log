8b526aa973cdc3670d457050162129678b556866
kimywann <Kimywan10@gmail.com>
2025-06-17 22:04:08 +0900

    수정

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Kim YoungWan",
-    image: "1296305.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/1296305.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "",
     email: "kimywan10@gmail.com",
